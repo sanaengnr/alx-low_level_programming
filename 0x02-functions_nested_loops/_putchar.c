6fe6f9678bc04371e8b9d8b10c1f566db9b21c49
sanae <sanaeoush@gmail.com>
2023-04-25 14:19:26 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,20 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - prints Holberton as a message.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (success )
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	int str []={95,112,117,116,99,104,97,114};
+	int count,SZ;
+
+	SZ= sizeof(str)/sizeof(int);
+	for (count = 0;count < SZ; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return(0);
 }
